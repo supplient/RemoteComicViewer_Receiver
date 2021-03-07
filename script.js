a00c9d1acd55b801ed4244df8d46d62d84d8df47
supplient <437566830@qq.com>
2021-03-07 15:46:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket("ws://81.70.248.180:17692");
+const socket = new WebSocket("wss://81.70.248.180:17692");
 // const socket = new WebSocket("ws://106.39.42.234:16453");
 
 socket.addEventListener("open", function(event) {
